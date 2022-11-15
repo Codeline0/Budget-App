6ded1b6f2f44edad815e0e2eb65069f1e373198e
tonycc <tonycamezcha@gmail.com>
2022-11-15 14:28:34 -0600

    feat: implemented __repr__ method to Category class

--- budget.py
+++ budget.py
@@ -4,6 +4,23 @@ class Category:
         self.ledger = []
         self.balance = 0.0
     
+    def __repr__(self) -> str:
+        asteriscs = int((30 - len(self.category)) / 2)
+        header = '*' * asteriscs + '{}'.format(self.category) + '*' * asteriscs + '\n'
+        listing = ''
+        for element in self.ledger:
+            description = element['description']
+            amount = '%.2f' % element['amount']
+            space_between = 30 - len(amount) - len(description)
+            if len(description) > 23:
+                space_between = 7 - len(amount)
+                listing += '{}'.format(description[:23]) + ' ' * space_between +'{}'.format(amount) + '\n'
+            else: 
+                listing += '{}'.format(description) + ' ' * space_between +'{}'.format(amount) + '\n'
+        total = 'Total: {:.2f}'.format(self.balance)
+        printed = header + listing + total
+        return printed
+
     def deposit(self, amount, description=''):
         self.ledger.append({'amount': amount, 'description': description})
         self.balance += amount
